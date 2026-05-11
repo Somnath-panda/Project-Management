0195585f6b8117397633e4951fbd783b43094f31
Somnath <somnathpanda885@gmail.com>
2026-05-11 23:43:04 +0530

    Integrated Clerk auth, Neon DB, Prisma ORM, and Inngest setup

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
+)
