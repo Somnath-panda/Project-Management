1968025907a4bce5dcad4adef2ad17e6bc1b699e
Somnath <somnathpanda885@gmail.com>
2026-06-21 17:29:02 +0530

    Backend completed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
+import projectRouter from './routes/projectRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
+import commentRouter from './routes/commentRoutes.js';
 
 const app = express();
 
@@ -25,6 +28,9 @@ app.get('/', (req,res) => res.send('Server is live!'));
 
 // Routes
 app.use('/api/workspaces', protect, workspaceRouter);
+app.use("/api/projects", protect,projectRouter)
+app.use("/api/tasks", protect, taskRouter)
+app.use("/api/comments", protect, commentRouter)
 
 if (process.env.NODE_ENV !== 'production') {
  const PORT = process.env.PORT || 5000;
